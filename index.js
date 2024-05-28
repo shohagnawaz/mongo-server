74def91d38df0872625ff74a19552fcc0f5d788a
shohag nawaz <snawazdigital@gmail.com>
2024-05-28 06:33:24 +0600

    finished

--- index.js
+++ index.js
@@ -29,6 +29,27 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     });
+    app.get('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const user = await userCollection.findOne(query);
+        res.send(user);
+    });
+    app.put('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const user = req.body;
+        console.log(id, user);
+        const filter = {_id: new ObjectId(id)};
+        const options = {upsert: true};
+        const updatedUser = {
+          $set: {
+            name: user.name, 
+            email: user.email
+          }
+        }
+        const result = await userCollection.updateOne(filter, updatedUser, options);
+        res.send(result);
+    });
     app.post('/users', async(req, res) => {
         const user = req.body;
         console.log('new user', user);
